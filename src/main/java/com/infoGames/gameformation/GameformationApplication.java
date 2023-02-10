422d625065e8f17763c4b5883b971d5d35d6f4ac
jordy muniz <jordy.muniz96@gmail.com>
2023-02-10 10:46:46 -0600

    added project

---
+++ src/main/java/com/infoGames/gameformation/GameformationApplication.java
@@ -0,0 +1,13 @@
+package com.infoGames.gameformation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameformationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameformationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/infoGames/gameformation/GameformationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infoGames.gameformation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameformationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
